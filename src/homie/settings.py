75e13d8922a812b83acbcd5c78b08cdd18894d71
daniel.kiermeier <daniel.kiermeier@check24.de>
2024-02-20 19:03:43 +0100

    Update static url

--- src/homie/settings.py
+++ src/homie/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
+
 import mimetypes
 import os
 from pathlib import Path
@@ -139,8 +140,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = str(BASE_DIR / "static")
+STATIC_URL = "static/api/"
+STATIC_ROOT = str(BASE_DIR / "static" / "api")
 STATICFILES_DIRS = ()
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
